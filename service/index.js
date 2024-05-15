06833fce734996c0567add05df043cc35d3bae86
Huey <huey@kolowich.org>
2024-05-15 15:23:29 -0600

    generate-video-token endpoint handling

--- service/index.js
+++ service/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('./build'));
 
+app.get('/generate-video-token', (req, res) => {
+  const options = { 
+      expiresIn: '120m', 
+      algorithm: 'HS256' 
+  };
+
+  const payload = {
+      apikey: process.env.VIDEOSDK_API_KEY,
+      permissions: ['allow_join']
+  };
+
+  const videoToken = jwt.sign(payload, process.env.VIDEOSDK_SECRET_KEY, options);
+
+  res.json({ videoToken });
+});
+
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
+  console.log(`Listening on port ${port}`);
+});
