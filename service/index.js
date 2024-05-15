64f2a94335f869ed5322035ffb7dc7fc249fe5e4
Huey <huey@kolowich.org>
2024-05-14 18:37:58 -0600

    fixed directory path to static files

--- service/index.js
+++ service/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('../build'));
+app.use(express.static('./build'));
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
