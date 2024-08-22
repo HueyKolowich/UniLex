3c03cf716dec522aa11d616b4c6668382d6f3add
Huey <huey@kolowich.org>
2024-08-22 14:14:43 -0600

    teacher analytics dashboard

--- src/teacher/TMC.js
+++ src/teacher/TMC.js
@@ -11,6 +11,7 @@ function TMCBody({ bringBackToLogin }) {
     const [modalOpen, setModalOpen] = useState(false);
     const [selectedStudent, setSelectedStudent] = useState(null);
     const [studentData, setStudentData] = useState([]);
+    const [classStats, setClassStats] = useState({ avgComfort: 0, avgHelpfulness: 0, conversationPercentage: 0 });
 
     return (
         <div>
@@ -24,14 +25,18 @@ function TMCBody({ bringBackToLogin }) {
                 <AssignmentView />
                 :
                 <div className="container-fluid">
-                    <div className="row my-5 mx-2">
+                    <div className="row my-3 mx-2">
                         <div className="col">
-                            <Table 
-                                bringBackToLogin={bringBackToLogin} 
-                                setSelectedStudent={setSelectedStudent}
-                                setModalOpen={setModalOpen}
-                                setStudentData={setStudentData}
-                            />
+                            <div className="d-flex flex-column mb-4">
+                                <ClassStats classStats={classStats} />
+                                <Table 
+                                    bringBackToLogin={bringBackToLogin} 
+                                    setSelectedStudent={setSelectedStudent}
+                                    setModalOpen={setModalOpen}
+                                    setStudentData={setStudentData}
+                                    setClassStats={setClassStats}
+                                />
+                            </div>
                         </div>
                         <div className="col">
                             <AssignmentOption setIsConfiguringAssignment={setIsConfiguringAssignment} setIsViewingAssignment={setIsViewingAssignment} />
@@ -47,6 +52,39 @@ function TMCBody({ bringBackToLogin }) {
                     />
                 </div>
             }
+        </div>
+    );
+}
+
+function ClassStats({ classStats }) {
+    const { avgComfort, avgHelpfulness, conversationPercentage } = classStats;
+
+    return (
+        <div className="d-flex justify-content-evenly text-center mt-1 mb-3">
+            <div className="card class-stats mx-3">
+                <div className="card-header">
+                    Avg. Helpfulness
+                </div>
+                <div className="card-body primary">
+                    {avgHelpfulness.toFixed(1)}
+                </div>
+            </div>
+            <div className="card class-stats mx-3">
+                <div className="card-header">
+                    Avg. Comfort
+                </div>
+                <div className="card-body">
+                    {avgComfort.toFixed(1)}
+                </div>
+            </div>
+            <div className="card class-stats mx-3">
+                <div className="card-header">
+                    Weekly Participation Rate
+                </div>
+                <div className="card-body">
+                    {conversationPercentage.toFixed(1)}%
+                </div>
+            </div>
         </div>
     );
 }
@@ -120,9 +158,10 @@ function CardHeader({text}) {
     );
 }
 
-function Table({ bringBackToLogin, setSelectedStudent, setModalOpen, setStudentData }) {
+function Table({ bringBackToLogin, setSelectedStudent, setModalOpen, setStudentData, setClassStats }) {
     const [studentList, setStudentList] = useState([]);
     const [searchQuery, setSearchQuery] = useState("");
+    const [sortConfig, setSortConfig] = useState({ key: "lastname", direction: "ascending" });
 
     useEffect(() => {
         const getStudentList = async () => {
@@ -133,8 +172,28 @@ function Table({ bringBackToLogin, setSelectedStudent, setModalOpen, setStudentD
                     return;
                 }
     
-                const data = await response.json();
-                setStudentList(data.studentList.sort((a, b) => a.lastname.localeCompare(b.lastname)));
+                const studentListData = await response.json();
+    
+                const studentList = await Promise.all(
+                    studentListData.studentList.map(async (student) => {
+                        const studentInfoResponse = await fetch(`/student-table-info?username=${student.username}`);
+                        const studentInfoData = await studentInfoResponse.json();
+
+                        const formatValue = (value) => value === "NA" ? "" : value.toFixed(1);
+    
+                        return {
+                            username: student.username,
+                            firstname: student.firstname,
+                            lastname: student.lastname,
+                            rating: formatValue(studentInfoData.rating),
+                            recent: studentInfoData.recent,
+                            score: formatValue(studentInfoData.score),
+                        };
+                    })
+                );
+    
+                setStudentList(studentList);
+                calculateStats(studentList);
             } catch (error) {
                 console.error("Error fetching the student list:", error);
             }
@@ -142,33 +201,76 @@ function Table({ bringBackToLogin, setSelectedStudent, setModalOpen, setStudentD
     
         getStudentList();
     }, []);
+    
+    const calculateStats = (studentList) => {
+        if (studentList.length === 0) return;
 
-    const filteredStudentList = studentList.filter(student =>
+        const calculateAverage = (studentList, field) => {
+            const filteredList = studentList.filter(student => student[field] !== "" && !isNaN(student[field]));
+            
+            if (filteredList.length === 0) return 0;
+        
+            const total = filteredList.reduce((acc, student) => acc + Number(student[field]), 0);
+            return total / filteredList.length;
+        };
+        
+        const avgComfort = calculateAverage(studentList, "rating");
+        const avgHelpfulness = calculateAverage(studentList, "score");                
+
+        const startOfWeek = new Date();
+        const dayOfWeek = (startOfWeek.getDay() + 6) % 7;
+        startOfWeek.setDate(startOfWeek.getDate() - dayOfWeek);
+
+        const recentMeetingsCount = studentList.filter(student => new Date(student.recent).getTime() >= startOfWeek.getTime()).length;
+        const conversationPercentage = (recentMeetingsCount / studentList.length) * 100;
+
+        setClassStats({ avgComfort, avgHelpfulness, conversationPercentage });
+    };
+
+    const sortedStudentList = [...studentList].sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+            return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+            return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+    });
+
+    const filteredStudentList = sortedStudentList.filter(student =>
         student.firstname.toLowerCase().includes(searchQuery.toLowerCase()) ||
         student.lastname.toLowerCase().includes(searchQuery.toLowerCase())
     );
 
+    const handleSort = (key) => {
+        let direction = "ascending";
+        if (sortConfig.key === key && sortConfig.direction === "ascending") {
+            direction = "descending";
+        }
+        setSortConfig({ key, direction });
+    };
+
     return (
         <div className="panel">
             <div className="d-flex">
-                <i className="bi bi-search" style={{padding: "0.5rem"}}/>
-                <input 
-                    type="text" 
-                    placeholder="Search by first or last name..." 
+                <i className="bi bi-search" style={{ padding: "0.5rem" }} />
+                <input
+                    type="text"
+                    placeholder="Search by first or last name..."
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
                     className="search-input"
                 />
             </div>
-            <div className="scroll-wrapper">
+            <div className="scroll-wrapper scroll-wrapper-table">
                 <table className="table table-borderless">
-                    <TableHead />
+                    <TableHead handleSort={handleSort} />
                     <tbody>
                         {filteredStudentList.map((student, index) => (
-                            <TableBodyItem 
-                                key={index} 
-                                index={index} 
-                                student={student} 
+                            <TableBodyItem
+                                key={index}
+                                index={index}
+                                student={student}
                                 setSelectedStudent={setSelectedStudent}
                                 setModalOpen={setModalOpen}
                                 setStudentData={setStudentData}
@@ -181,24 +283,22 @@ function Table({ bringBackToLogin, setSelectedStudent, setModalOpen, setStudentD
     );
 }
 
-function TableHead() {
+function TableHead({ handleSort }) {
     return (
         <thead>
             <tr className="table-no-top-border">
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Comfort</th>
-                <th scope="col">Helpfulness</th>
-                <th scope="col">Last Meeting</th>
+                <th scope="col" onClick={() => handleSort("firstname")}>First</th>
+                <th scope="col" onClick={() => handleSort("lastname")}>Last</th>
+                <th scope="col" onClick={() => handleSort("rating")}>Comfort</th>
+                <th scope="col" onClick={() => handleSort("score")}>Helpfulness</th>
+                <th scope="col" onClick={() => handleSort("recent")}>Last Meeting</th>
             </tr>
         </thead>
     );
 }
 
 function TableBodyItem({ index, student, setSelectedStudent, setModalOpen, setStudentData }) {
-    const [studentTableInfo, setStudentTableInfo] = useState({ rating: "", recent: "", score: "" });
-
     const bringUpStudentData = async (student) => {
         try {
             const response = await fetch(`/submissions?username=${student.username}`);
@@ -213,29 +313,14 @@ function TableBodyItem({ index, student, setSelectedStudent, setModalOpen, setSt
         setModalOpen(true);
     };
 
-    useEffect(() => {
-        const getStudentTableInfo = async (student) => {
-            try {
-                const response = await fetch(`/student-table-info?username=${student.username}`);
-    
-                const data = await response.json();
-                setStudentTableInfo({ rating: data.rating, recent: data.recent, score: data.score });
-            } catch (error) {
-                console.error("Error fetching table data for the selected student:", error);
-            }
-        };
-
-        getStudentTableInfo(student);
-    }, [student]);
-
     return (
         <tr className="rowItem" onClick={() => bringUpStudentData(student)}>
             <th scope="row">{index + 1}</th>
             <td>{student.firstname}</td>
             <td>{student.lastname}</td>
-            <td>{studentTableInfo.rating}</td>
-            <td>{studentTableInfo.score}</td>
-            <td>{formatGMTDate(studentTableInfo.recent).date}</td>
+            <td>{student.rating}</td>
+            <td>{student.score}</td>
+            <td>{formatGMTDate(student.recent).date}</td>
         </tr>
     );
 }
